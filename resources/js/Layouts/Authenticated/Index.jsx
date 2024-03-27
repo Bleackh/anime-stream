4dc4505e6f3fba90ead3d911fb6d371f5c570abb
Bleackh <faldhi.works@gmail.com>
2024-03-27 14:48:52 +0800

    feat: slicing dashboard

--- resources/js/Layouts/Authenticated/Index.jsx
+++ resources/js/Layouts/Authenticated/Index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
 
 export default function Authenticated({ children }) {
     return (
         <>
             <div className="mx-auto max-w-screen hidden lg:block">
                 {/* START: Sidebar */}
-                <Sidebar></Sidebar>
+                <Sidebar />
                 {/* END: Sidebar */}
 
                 {/* START: Content */}
                 <div className="ml-[300px] px-[50px]">
                     <div className="py-10 flex flex-col gap-[50px]">
+                        {/* START: Topbar */}
+                        <Topbar />
+                        {/* END: Topbar */}
                         <main>{children}</main>
                     </div>
                 </div>
--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,12 +1,64 @@
 import React from "react";
 import Authenticated from "@/Layouts/Authenticated/Index";
 import { Link, Head } from "@inertiajs/react";
+import Flickity from "react-flickity-component";
+import FeaturedMovie from "@/Components/FeaturedMovie";
+import MoviceCard from "@/Components/MoviceCard";
 
 export default function Dashboard() {
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
     return (
         <>
-            <Head title="Dashboard"></Head>
-            <Authenticated></Authenticated>
+            <Head title="Dashboard">
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+            </Head>
+            <Authenticated>
+                <div>
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Featured Movies
+                    </div>
+
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[1, 2, 3, 4].map((i) => (
+                            <FeaturedMovie
+                                key={i}
+                                slug="the-batman-in-love"
+                                name={`The Batman in ${i}`}
+                                thumbnail="https://www.picsum.photos/id/1/300/300"
+                                category="Action • Thriller"
+                                rating={i + 1}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
+                <div className="mt-[50px]">
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Browse
+                    </div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[1, 2, 3, 4, 5, 6].map((i) => (
+                            <MoviceCard
+                                key={i}
+                                slug="the-superman-in-love"
+                                name={`The Supermain in ${i}`}
+                                thumbnail="https://www.picsum.photos/id/2/300/300"
+                                category="Action • Superhero"
+                            />
+                        ))}
+                    </Flickity>
+                </div>
+            </Authenticated>
         </>
     );
 }
